810a2805497ca9c71de90e3dc1ad944273fb0482
Local Al <alexander.s.woolley@gmail.com>
2020-12-13 22:17:06 -0800

    fixed append buttons

--- script.js
+++ script.js
@@ -10,19 +10,36 @@ searchbtn.click(function() {
     var savedCities = JSON.parse(localStorage.getItem("citySearchesArray")) || [];
     savedCities.push(recentCity);
     localStorage.setItem("citySearchesArray", JSON.stringify(savedCities));
-}); 
-
 
 var citiesDisplay = (JSON.parse(localStorage.getItem("citySearchesArray")))
-
- for (let i = 0; i < citiesDisplay.length; i++) {
 
     var cityButton=$('<input/>').attr({
         type: "button",
         id: "cityName",
         class: "btn btn-success  btn-outline-light" ,
-        value: citiesDisplay[i] 
+        value: citiesDisplay[citiesDisplay.length-1] 
     })
     appendCitiesHere.append(cityButton)
- }
 
+ cityInput.val("")
+
+}); 
+
+
+
+
+ //weather API
+
+//  var city = cityInput.val()
+//  var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=c34d74a8d0eb82fb6170c0fa6b59b0e8"
+
+// function weather(){
+//     $.ajax({
+//         url: url,
+//         method: "GET"
+//     })
+//     .then(function(response){
+//    // var responseName = response.name;
+//     console.log(response)
+//     })
+// } 
