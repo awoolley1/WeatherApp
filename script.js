70093dc835cbaa966ca515651f924c7f2bd689b2
Local Al <alexander.s.woolley@gmail.com>
2020-12-14 00:20:12 -0800

    UV API call added

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 var searchbtn = $("#search-btn")
 var cityInput = $("#city-input")
 var appendCitiesHere = $("#display-cities")
+var nameDate = $("#city-name-date")
+var temperature = $("#temp")
+var humidity = $("#humid")
+var windSpeed = $("#wind")
+var uv = $("#uv")
 
 
 //onclick search functionality
@@ -25,20 +30,53 @@ var citiesDisplay = (JSON.parse(localStorage.getItem("citySearchesArray")))
 
  //weather API
 var city = citiesDisplay[citiesDisplay.length-1]
-var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=c34d74a8d0eb82fb6170c0fa6b59b0e8"
+var urlWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=c34d74a8d0eb82fb6170c0fa6b59b0e8";
 
 function weatherGet(){
     $.ajax({
-        url: url,
+        url: urlWeather,
         method: "GET"
     })
     .then(function(response){
-    console.log (city)
+
+    var date = new Date((response.dt)*1000)
+    var month = date.getMonth()
+    var day = date.getDay()
+    console.log(date)
+    console.log(month)
+    
     console.log(response)
+    nameDate.text((response.name) + " " + (month) + " " + (day))
+    temperature.text("Temperature today is: " + (((response.main.temp)-273.15)*1.8+32).toFixed(1) + " degrees Fahrenheit")
+    windSpeed.text("Wind speed today is: " + response.wind.speed + " MPH")
+    humidity.text("Humidity today is: " + response.main.humidity + "%")
     })
+
+    var lat = response.coord.lat;
+    var lon = response.coord.lon;
+
+    var urlUV = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=c34d74a8d0eb82fb6170c0fa6b59b0e8";
+
+    function UVGet(){
+        $.ajax({
+            url: urlUV,
+            method: "GET"
+        })
+        .then(function(response){
+            consolelog(response)
+    
+        uv.text("UV index today is:" + "")
+      
+        })
+    
+    } 
+
 } 
 
+
+
 weatherGet()
+//UVGet()
 
 }); 
 
